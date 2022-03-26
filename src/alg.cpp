552db2566cc6a0e16afcb4426837eb995bba72e2
OlegWellDone <63550097+OlegWellDone@users.noreply.github.com>
2022-03-26 12:36:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -32,5 +32,34 @@ int countPairs2(int *arr, int len, int value) {
   return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int i, j, k, mid, nachalo, value1;
+  while (arr[nachalo] < value) {
+    value1 = value - arr[nachalo];
+  i = 0;
+  j = len - 1;
+  k = 0;
+  mid = (j - 1) / 2;
+  while (arr[j] > value1) {
+    mid = (j - 1) / 2;
+    if (arr[mid] == value1) {
+      k = k + 1;
+      j = mid;
+      break;
+    } else {
+     j = mid;
+    }
+  }
+    i = 1;
+    while ((arr[j + i] == value1) && ((j + i) <= len)) {
+      k = k + 1;
+      i = i + 1;
+      }
+    i = 1;
+      while ((arr[j - i] == value1) && ((j - i) >= 0)) {
+      k = k + 1;
+      i = i + 1;
+      }
+}
+  nachalo = nachalo + 1;
+  return k;
 }
