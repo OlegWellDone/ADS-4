d184716936564315d2b95fcc5ae17bdef67fe1a8
OlegWellDone <63550097+OlegWellDone@users.noreply.github.com>
2022-03-26 23:07:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -28,29 +28,45 @@ int countPairs2(int *arr, int len, int value) {
       return count;
 }
 
-int bin_search(int* arr, int begin, int len, int value) {
-    int count = 0, i = begin, end = len - 1, midd, tmp;
-    while (i <= end) {
-        midd = i + (end - i) / 2;
-        if (arr[midd] == value && midd != begin && midd != len) {
-            count++;
-            tmp = midd;
-            while (arr[--midd] == value && midd > begin) count++;
-            while (arr[++tmp] == value && tmp < len) count++;
-            break;
-        } else if (arr[midd] > value) {
-            end = midd - 1;
-        } else {
-            i = midd + 1;
-        }
+int cbinsearch(int *arr, int size, int value, int begin) {
+  int i = begin;
+  int j = size - 1;
+  int k = 0;
+  int mid = (j - 1) / 2;
+    if (arr[j] == value) {
+      k = k + 1;
     }
-    return count;
+  while (arr[j] > value) {
+    mid = (j - 1) / 2;
+    if (arr[mid] == value) {
+      k = k + 1;
+      j = mid;
+      break;
+    } else {
+     j = mid;
+    }
+  }
+    i = 1;
+    while ((arr[j + i] == value) && ((j + i) <= size)) {
+      k = k + 1;
+      i = i + 1;
+      }
+    i = 1;
+      while ((arr[j - i] == value) && ((j - i) >= 0)) {
+      k = k + 1;
+      i = i + 1;
+      }
+  if (k != 0) {
+    return k;
+  } else {
+  return 0;
+  }
 }
 
 int countPairs3(int *arr, int len, int value) {
 int count = 0;
   for (int i = 0; i < len; i++) {
-  count = count +  bin_search(arr, i, len, value - arr[i]);
+  count = count +  cbinsearch(arr, len, value - arr[i], i);
   }
   return count;
 }
