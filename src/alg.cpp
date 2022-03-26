b243fb36f34a808ca86e97c3be540f9b5818a9ea
OlegWellDone <63550097+OlegWellDone@users.noreply.github.com>
2022-03-26 12:09:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,34 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count;
+  for (int i = 0; i < len - 1; i++ ) {
+    for (int k = i + 1; k < len; k++) {
+      if (arr[i] + arr[k] = value) {
+        count = count + 1;
+      }
+    }
+  }
+  return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int konec, nachalo;
+  nachalo = 0;
+  for (konec = len; konec > ((len/2) - 1); konec--) {
+    if (arr[nachalo] + arr[konec] > value) {
+      nachalo = nachalo + 1;
+    } else if (arr[nachalo] + arr[konec] <= value) {
+      break;
+    }
+  }
+  for (int i = nachalo; i < konec - 1; i++) {
+    for (int k = i + 1; k < konec; k++) {
+      if (arr[i] + arr[k] = value) {
+        count = count + 1;
+      }
+    }
+  }
+
+  return count;
 }
 int countPairs3(int *arr, int len, int value) {
   return 0;
