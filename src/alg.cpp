6cb3ac5b16e273a70b417d5a8fe6191eee66bf35
OlegWellDone <63550097+OlegWellDone@users.noreply.github.com>
2022-03-26 13:07:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -15,11 +15,9 @@ int countPairs2(int *arr, int len, int value) {
   int count;
   nachalo = 0;
   for (konec = len; konec > ((len/2) - 1); konec--) {
-    if (arr[nachalo] + arr[konec] > value) {
-      nachalo = nachalo + 1;
-    } else if (arr[nachalo] + arr[konec] <= value) {
+    if(arr[konec] <= value) {
       break;
-    }
+    }   
   }
   for (int i = nachalo; i < konec - 1; i++) {
     for (int k = i + 1; k < konec; k++) {
